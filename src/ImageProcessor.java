b217b9d882a45abbc91850d71eea40cf88e65fa1
Bill Scott <bill48893@gmail.com>
2018-01-04 07:58:11 -0500

    Added more code

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.opencv.core.Core;
@@ -20,6 +21,11 @@ public class ImageProcessor {
 	private final int BLUR_SIZE = 3;
 	private final int THRESH = 100;
 	private final int MIN_CONTOUR_AREA = 10;
+	private final int MAX_ROTATED_REC_ANGLE = 5;
+	private final double ASPECT_RATIO_MIN = 0.3;
+	private final double ASPECT_RATIO_MAX = 0.7;
+	private final int MIN_RECT_HEIGHT = 25;
+	private final int MAX_RECT_HEIGHT = 999;
 
 	private Imshow originalImageWindow;
 	private Imshow countoursImageWindow;
@@ -29,6 +35,11 @@ public class ImageProcessor {
 	private boolean showMarkupImage = false;
 	private String lightColor = "blue";
 	private int cycles = 0;
+	
+	private Mat originalImage;
+	private Mat processedImage;
+	private Mat markupImage;
+	private Mat contourImage;
 
 	private List<MatOfPoint> contours;
 
@@ -60,9 +71,13 @@ public class ImageProcessor {
 	}
 
 	public void ProcessImage(Mat originalImage) {
-		Mat processedImage = originalImage;
-		Mat markupImage = originalImage;
+		this.originalImage = originalImage;
+		processedImage = originalImage;
+		markupImage = originalImage;
+		contourImage = originalImage;
 		List<ContourData> contourDataList = new ArrayList<ContourData>();
+		List<ContourData> potentialContourDataList = new ArrayList<ContourData>();
+		List<ContourData> acceptedContourDataList = new ArrayList<ContourData>();
 
 		cycles++;
 		if (originalImageWindow != null)
@@ -74,6 +89,10 @@ public class ImageProcessor {
 		processedImage = applyColorFilter(processedImage);
 
 		contourDataList = findCoutours(processedImage);
+		
+		potentialContourDataList = drawBouningBoxes(contourDataList);
+		
+		acceptedContourDataList = findAcceptedContours(potentialContourDataList);
 
 		if (countoursImageWindow != null)
 			countoursImageWindow.showImage(processedImage);
@@ -90,7 +109,6 @@ public class ImageProcessor {
 		Mat newImage = new Mat();
 		image.convertTo(newImage, -1, alpha, beta);
 		return newImage;
-
 	}
 
 	private Mat blurImage(Mat image) {
@@ -164,6 +182,88 @@ public class ImageProcessor {
 		return contourDataList;
 	}
 
+	private List<ContourData>  drawBouningBoxes(List<ContourData> contourDataList) {
+		List<ContourData> potentialContourDataList = new ArrayList<ContourData>();
+	    for (int i = 0; i < contourDataList.size(); i++)
+	    {
+	    	Rect boundRect = contourDataList.get(i).getBoundRect();
+	    	RotatedRect rotatedRect = contourDataList.get(i).getRotatedRect();
+	    	MatOfPoint contour = contourDataList.get(i).getContour();
+	    		    	
+	        if (boundRect.height > MIN_RECT_HEIGHT && boundRect.height < MAX_RECT_HEIGHT && rotatedRect.angle < MAX_ROTATED_REC_ANGLE)
+	        {
+	            double aspect_ratio = (double) boundRect.width / (double) boundRect.height;
+
+	            if (aspect_ratio >= ASPECT_RATIO_MIN && aspect_ratio <= ASPECT_RATIO_MAX)
+	            {
+	            	potentialContourDataList.add(new ContourData(contour, boundRect, rotatedRect));
+
+	            	List<MatOfPoint> coutours = new ArrayList<MatOfPoint>();
+	            	coutours.add(contour);
+	            	
+	                if (showCountoursImage)
+	                {
+	                    Scalar color = new Scalar(192, 192, 192);
+	                    Imgproc.drawContours(contourImage, contours, i, color);
+	                    color = new Scalar(255, 0, 0);
+	                    Imgproc.rectangle(contourImage, boundRect.tl(), boundRect.br(), color, 2, 8, 0);
+	                }
+	                if (showOriginalImage)
+	                {
+	                    Scalar color = new Scalar(192, 192, 192);
+	                    Imgproc.drawContours(originalImage, contours, i, color);
+	                    color = new Scalar(255, 0, 0);
+	                    Imgproc.rectangle(originalImage, boundRect.tl(), boundRect.br(), color, 2, 8, 0);
+	                }
+	            }
+	        }
+	    }
+	    return potentialContourDataList;
+
+	}
+	
+	private List<ContourData> findAcceptedContours(List<ContourData> potentialContourDataList)
+	{
+		List<ContourData> acceptedContourDataList = new ArrayList<ContourData>();
+	    int last_x = 0;
+	    int last_y = 0;
+	    for (int i = 0; i < potentialContourDataList.size(); i++)
+	    {
+	    	Rect boundRect = potentialContourDataList.get(i).getBoundRect();
+	    	RotatedRect rotatedRect = potentialContourDataList.get(i).getRotatedRect();
+	    	MatOfPoint contour = potentialContourDataList.get(i).getContour();
+	    	
+	        double MAX_X_OFF = 1.7 * boundRect.height;
+	        double MIN_X_OFF = .8 * boundRect.height;
+	        double MAX_Y_OFF = .15 * boundRect.height;
+
+	        for (int x = 0; x < potentialContourDataList.size(); x++)
+	        {
+	        	Rect otherPotentialRect = potentialContourDataList.get(x).getBoundRect();
+	            int delta_x = Math.abs(boundRect.x - otherPotentialRect.x);
+	            int delta_y = Math.abs(boundRect.y - otherPotentialRect.y);
+	            if (x != i && delta_x > 0 & delta_y < MAX_Y_OFF)
+	            {
+	                double height_diff_pct = (double) Math.abs(boundRect.height - otherPotentialRect.height) / (double) boundRect.height;
+	                double width_diff_pct = (double) Math.abs(boundRect.width - otherPotentialRect.width) / (double) boundRect.width;
+
+	                if (last_x != otherPotentialRect.x && last_y != otherPotentialRect.y)
+	                {
+	                    if (delta_x >= MIN_X_OFF && delta_x <= MAX_X_OFF && height_diff_pct < 0.1 && width_diff_pct < 0.1)
+	                    {
+	                        last_x = otherPotentialRect.x;
+	                        last_y = otherPotentialRect.y;
+
+	                        acceptedContourDataList.add(new ContourData(contour, boundRect, rotatedRect));
+	                        break;
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    return acceptedContourDataList;
+	}
+	
 	public int getCycles() {
 		return cycles;
 	}
